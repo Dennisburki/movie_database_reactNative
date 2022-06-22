6bdcf53ca91c25f2626ab8f36c1b1634b27c12d9
Dennis <basketor30@hotmail.fr>
2022-06-22 14:23:15 +0200

    test

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native'
+import * as React from 'react'
+import HomeScreen from './src/screens/HomeScreen'
+import StackNav from './src/navigation/StackNav'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1}}>
+      <StackNav/>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
